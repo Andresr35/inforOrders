66f843fb1582c50283024e95b675ad6e6278d1dd
Andres Ruiz <Andres.ruiz3561@gmail.com>
2022-08-26 15:19:42 -0700

    Documented Utils.py

--- inforOrders/Utils.py
+++ inforOrders/Utils.py
@@ -1,13 +1,22 @@
+''' 
+This file is meant to hold parser functions that read/write csv and json files. 
+
+Here the parser for the input csv will be changed to a dict,
+    orders.csv will be created here,
+    and reading the names.json file into a dict.
+'''
+
+
 import csv
 import json
+
 import pandas as pd
 
 
 class csvUtils:
-    """creates an object from shopify csv 
+    """ Parser functions for reading input csv and export csv with finished orders
     """
 
-    
     def readCSV(csvInput):
         """Reads shpify csv to give back an object to allow for easier looping through orders and line items
 
@@ -18,74 +27,88 @@ class csvUtils:
             dict: dictionary holding orders
         """
 
-        rawCSV =pd.read_csv(csvInput)
+        # Using pandas library to help parse csv
+        rawCSV = pd.read_csv(csvInput)
         newDict = {}
-        for index,row in rawCSV.iterrows(): #gets all rows and just the names with their index
 
+        # Going through each row in csv and making a key
+        for index, row in rawCSV.iterrows():
+            # If statement here deals with the way infor exports csv's
+            # orders can span multiple rows, need a way to parse that into a dict
             if('order' not in locals()):
                 newDict.update({
-                    row['Name']:{
-                    'shippingName':row["Shipping Name"],
-                    'shippingStreet':row["Shipping Street"],
-                    'shippingCity':row["Shipping City"],
-                    'shippingZip':row["Shipping Zip"].replace("'",""),
-                    'shippingState':row["Shipping Province"],
-                    'shippingCountry':row["Shipping Country"],
-                    'shippingAmount':row["Shipping"],
-                    'discount':row["Discount Amount"],
-                    'method':row["Payment Method"],
-                    'lineItems':[{
-                        'sku':row["Lineitem sku"],
-                        'price':row["Lineitem price"],
-                        'quantity':row["Lineitem quantity"]
-                    }]
-                }})
+                    row['Name']: {
+                        'shippingName': row["Shipping Name"],
+                        'shippingStreet': row["Shipping Street"],
+                        'shippingCity': row["Shipping City"],
+                        'shippingZip': row["Shipping Zip"].replace("'", ""),
+                        'shippingState': row["Shipping Province"],
+                        'shippingCountry': row["Shipping Country"],
+                        'shippingAmount': row["Shipping"],
+                        'discount': row["Discount Amount"],
+                        'method': row["Payment Method"],
+                        'lineItems': [{
+                            'sku': row["Lineitem sku"],
+                            'price':row["Lineitem price"],
+                            'quantity':row["Lineitem quantity"]
+                        }]
+                    }})
             elif(row["Name"] == order["Name"]):
-                x:dict = newDict.get(row["Name"])
-                y:list=x.get('lineItems')
+                x: dict = newDict.get(row["Name"])
+                y: list = x.get('lineItems')
                 y.append(
                     {
-                        'sku':row["Lineitem sku"],
-                        'price':row["Lineitem price"],
-                        'quantity':row["Lineitem quantity"]
+                        'sku': row["Lineitem sku"],
+                        'price': row["Lineitem price"],
+                        'quantity': row["Lineitem quantity"]
                     })
             else:
-                 newDict.update({row["Name"]:{
-                    'shippingName':row["Shipping Name"],
-                    'shippingStreet':row["Shipping Street"],
-                    'shippingCity':row["Shipping City"],
-                    'shippingZip':str(row["Shipping Zip"]).replace("'",""),
-                    'shippingState':row["Shipping Province"],
-                    'shippingCountry':row["Shipping Country"],
-                    'shippingAmount':row["Shipping"],
-                    'discount':row["Discount Amount"],
-                    'method':row["Payment Method"],
-                    'lineItems':[{
-                        'sku':row["Lineitem sku"],
+                newDict.update({row["Name"]: {
+                    'shippingName': row["Shipping Name"],
+                    'shippingStreet': row["Shipping Street"],
+                    'shippingCity': row["Shipping City"],
+                    'shippingZip': str(row["Shipping Zip"]).replace("'", ""),
+                    'shippingState': row["Shipping Province"],
+                    'shippingCountry': row["Shipping Country"],
+                    'shippingAmount': row["Shipping"],
+                    'discount': row["Discount Amount"],
+                    'method': row["Payment Method"],
+                    'lineItems': [{
+                        'sku': row["Lineitem sku"],
                         'price':row["Lineitem price"],
                         'quantity':row["Lineitem quantity"]
                     }]
                 }})
-
             order = row
 
         return newDict
-        
-        
-    def createCSV(finishedOrders:list,failedOrders:list):
-        headings = ["Order","Status"]
-        file = open('orders.csv','w',encoding='UTF8')
+
+    def createCSV(finishedOrders: list, failedOrders: list):
+        """Creating a csv to export failed and finished orders
+
+        Args:
+            finishedOrders (list): strings of orders that finished sucessfully
+            failedOrders (list): str of orders that failed and need revision
+        """
+        # Setting up csv writer
+        headings = ["Order", "Status"]
+        file = open('../orders.csv', 'w', encoding='UTF8')
         writer = csv.writer(file)
 
+        # Writing the rows to csv
         writer.writerow(headings)
         for order in failedOrders:
-            writer.writerow([order,'Failed'])
+            writer.writerow([order, 'Failed'])
         for order in finishedOrders:
-            writer.writerow([order,'Finished'])
+            writer.writerow([order, 'Finished'])
 
+        # Finish the csv
         file.close()
-    # os.system('orders.csv')
+
+
 class jsonUtils:
+    """ Converts json to dict
+    """
 
     def readJSON() -> dict:
         """reads the countries json and gives back a dict with the code
@@ -93,6 +116,6 @@ class jsonUtils:
         Returns:
             dict: dict holding the code as the key and the name as the value
         """
-        with open('names.json') as json_file:
+        with open('./assets/names.json') as json_file:
             data = json.load(json_file)
             return data
