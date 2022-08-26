798fee15e11505caf70a29ce2ff764f0a38c64e7
Andres Ruiz <Andres.ruiz3561@gmail.com>
2022-08-26 14:57:28 -0700

    Cleaned web driver page

--- inforOrders/WebDriver.py
+++ inforOrders/WebDriver.py
@@ -1,37 +1,32 @@
-import os
-import sys
-from configparser import ConfigParser
+"""This page sets up the web driver to be used everywhere.
+NOTE: Selenium usually calls their webdriver driver.
+I called it web.
+
+
+"""
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-def resource_path(relative_path: str) -> str:
-    """gets the absolute path from the relative path
-
-    Args:
-        relative_path (str): in the local dir
-
-    Returns:
-        str: abs path
-    """
-    try:
-        base_path = sys._MEIPASS
-
-    except Exception:
-        base_path = os.path.dirname(__file__)
-    
-    return os.path.join(base_path, relative_path)
 
 class EnviromentSetUp(webdriver.Chrome):
 
     @classmethod
     def setUp(cls):
+        """This just sets up the options for the web driver. The web object can then be used to open
+        sites and other selenium methods.
+
+        Call closeWeb() to stop the driver.
+        """
+
         options = webdriver.ChromeOptions()
         options.add_argument("--start-maximized")
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-        cls.web = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        # ChromDriverManager handles the driver for chrome instead of
+        # requiring users to download the chrom driver on their own.
+        cls.web = webdriver.Chrome(service=ChromeService(
+            ChromeDriverManager().install()), options=options)
 
     @classmethod
     def closeWeb(cls):
